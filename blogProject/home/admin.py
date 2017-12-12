fd36eae621ee350deaf154585852ee2d5b8c1cd2
switchwg <1910436287@qq.com>
2017-12-12 09:28:37 +0800

    少量完善

--- blogProject/home/admin.py
+++ blogProject/home/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
 
-
 # Register your models here.
-from home.models import Article
+from home.models import Article, Category, Tag
 
 
 class ArticleAdmin(admin.ModelAdmin):
     # 页面中显示的选项
     list_display = ['id', 'title', 'excerpt', 'content', 'create_time',
-                    'modified_time',]
+                    'modified_time', ]
     # 每页显示的条数
     list_per_page = 10
 
 
-admin.site.register(Article, ArticleAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    """分类"""
+    list_display = ['name']
 
+
+class TagAdmin(admin.ModelAdmin):
+    """标签"""
+    list_display = ['name']
+
+
+# 注册
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- blogProject/home/models.py
+++ blogProject/home/models.py
@@ -7,39 +7,55 @@ class Category(models.Model):
     """
     分类名
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='分类')
+
+    def __str__(self):
+        # 返回类名
+        return self.name
 
     class Meta:
+        # 元类
         db_table = "Category"
+        verbose_name = "分类"
+        verbose_name_plural = "分类s"
 
 
 class Tag(models.Model):
     """
     标签
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='标签')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = "Tag"
+        verbose_name = "标签"
+        verbose_name_plural = "标签s"
 
 
 class Article(models.Model):
     """
     文章
     """
-
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name='标题')
     # 文章简介   blank=True 表示可以不写 没有的话　　此字段必须要有值
-    excerpt = models.CharField(max_length=200, blank=True)
-    content = models.TextField()
+    excerpt = models.CharField(max_length=200, blank=True, verbose_name='简介')
+    content = models.TextField(verbose_name='内容')
 
-    create_time = models.DateTimeField()
-    modified_time = models.DateTimeField()  # 修改时间
+    create_time = models.DateTimeField(verbose_name='建立时间')
+    modified_time = models.DateTimeField(verbose_name='修改时间')  # 修改时间
     # 分类外键
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, verbose_name='分类')
     # 多对多　文章和标签
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='标签')
 
-    # 元类重命名
+    def __str__(self):
+        return self.title
+
+    # 元类
     class Meta:
         db_table = "Article"
+        verbose_name = "文章"
+        verbose_name_plural = "文章s"
